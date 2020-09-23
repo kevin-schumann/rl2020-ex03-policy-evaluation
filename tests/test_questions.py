47cfad0085e337037a03c0c54641b24093cfe58f
Theresa Eimer <eimer@tnt.uni-hannover.de>
2020-09-23 11:38:37 +0200

    question test

---
+++ tests/test_questions.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestQuestions(unittest.TestCase):
+
+    def TestQuestions(self):
+        with open('observations.txt') as fh:
+            lines = fh.readlines()
+
+        self.assertTrue(lines[0].startswith('Dyn') or lines[0].startswith('dyn'))
+        self.assertTrue(lines[1].startswith('T') or lines[1].startswith('t') or lines[1].startswith('M') or lines[1].startswith('m'))
+
+if __name__ == '__main__':
+    unittest.main()
